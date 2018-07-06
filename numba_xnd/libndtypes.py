f1222d3c75e0742768ef43e1f8de7455adea1fcc
Saul Shanabrook <s.shanabrook@gmail.com>
2018-07-06 10:17:56 -0400

    Sttartt adding ndtypes

---
+++ numba_xnd/libndtypes.py
@@ -0,0 +1,50 @@
+from llvmlite import ir
+from llvmlite.ir import PointerType as ptr, global_context as context
+
+from numba.extending import models, register_model, lower_builtin
+from numba import types
+
+i8, i16, i32, i64 = map(ir.IntType, [8, 16, 32, 64])
+
+
+ndt_t = context.get_identified_type("_ndt")
+ndt_context_t = context.get_identified_type("_ndt_context")
+ndt_ndarray_t = context.get_identified_type("ndt_ndarray_t")
+
+
+class NdtType(types.Type):
+    def __init__(self):
+        super().__init__(name="Ndt")
+
+
+class NdtNdarrayType(types.Type):
+    def __init__(self):
+        super().__init__(name="NdtNdarray")
+
+
+class NdtContextType(types.Type):
+    def __init__(self):
+        super().__init__(name="NdtContext")
+
+
+ndt_type = NdtType()
+
+
+@register_model(ndt_type)
+class NdtModel(models.PrimitiveModel):
+    def __init__(self, dmm, fe_type):
+        be_type = ptr(ndt_t)
+        super().__init__(dmm, fe_type, be_type)
+
+
+def ndt_as_ndarray(a, t, ctx):
+    raise NotImplementedError()
+
+
+@lower_builtin(ndt_as_ndarray, NdtNdarrayType, NdtType, NdtContextType)
+def ndt_as_ndarray_impl(context, builder, sig, args):
+    xnd_ndim = builder.module.get_or_insert_function(
+        ir.FunctionType(i32, [ptr(ndt_ndarray_t), ptr(ndt_t), ptr(ndt_context_t)]),
+        name="ndt_as_ndarray",
+    )
+    return builder.call(xnd_ndim, [a.value for a in args])
