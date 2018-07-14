0b1762389bb90d05210e8a464eabfe9222806835
Saul Shanabrook <s.shanabrook@gmail.com>
2018-07-14 12:07:04 -0500

    Switch to using intrinsic in libndtypes (thanks Siu!)

--- numba_xnd/libndtypes/lowering.py
+++ numba_xnd/libndtypes/lowering.py
@@ -1,37 +1,60 @@
 from llvmlite import ir
 
-from numba.extending import lower_builtin, lower_getattr
+from numba.extending import lower_getattr, intrinsic
 from numba.targets.listobj import ListInstance
 from numba.targets.imputils import impl_ret_new_ref
 from numba import cgutils
+import numba.types
 
 from ..shared import ptr, int_, index, i64
-from . import api
 from . import types
 from . import llvm
 
 
-@lower_builtin(api.create_ndt_ndarray)
-def create_ndt_ndarray_impl(context, builder, sig, args):
-    return builder.alloca(llvm.ndt_ndarray_t)
+@intrinsic
+def create_ndt_ndarray(typingctx):
+    sig = types.ndt_ndarray_type()
+
+    def codegen(context, builder, sig, args):
+        return builder.alloca(llvm.ndt_ndarray_t)
+
+    return sig, codegen
 
 
-@lower_builtin(api.create_ndt_context)
-def create_ndt_context_impl(context, builder, sig, args):
-    return builder.alloca(llvm.ndt_context_t)
+@intrinsic
+def create_ndt_context(typingctx):
+    sig = types.ndt_context_type()
+
+    def codegen(context, builder, sig, args):
+        return builder.alloca(llvm.ndt_context_t)
+
+    return sig, codegen
 
 
-@lower_builtin(
-    api.ndt_as_ndarray, types.NdtNdarrayType, types.NdtType, types.NdtContextType
-)
-def ndt_as_ndarray_impl(context, builder, sig, args):
-    ndt_as_ndarray_ = builder.module.get_or_insert_function(
-        ir.FunctionType(
-            int_, [ptr(llvm.ndt_ndarray_t), ptr(llvm.ndt_t), ptr(llvm.ndt_context_t)]
-        ),
-        name="ndt_as_ndarray",
+@intrinsic
+def ndt_as_ndarray(typingctx, a, t, ctx):
+    if not (
+        isinstance(a, types.NdtNdarrayType)
+        and isinstance(t, types.NdtType)
+        and isinstance(ctx, types.NdtContextType)
+    ):
+        return
+
+    sig = numba.types.int32(
+        types.ndt_ndarray_type, types.ndt_type, types.ndt_context_type
     )
-    return builder.call(ndt_as_ndarray_, args)
+
+    def codegen(context, builder, sig, args):
+        ndt_as_ndarray_ = builder.module.get_or_insert_function(
+            ir.FunctionType(
+                int_,
+                [ptr(llvm.ndt_ndarray_t), ptr(llvm.ndt_t), ptr(llvm.ndt_context_t)],
+            ),
+            name="ndt_as_ndarray",
+        )
+        return builder.call(ndt_as_ndarray_, args)
+
+    return sig, codegen
 
 
 @lower_getattr(types.NdtType, "ndim")
@@ -44,6 +67,7 @@ def ndt_ndim_impl(context, builder, typ, value):
 
 @lower_getattr(types.NdtNdarrayType, "ndim")
 def ndt_ndarray_ndim_impl(context, builder, typ, value):
+    print(builder.module)
     get_ndt_ndarray_t_ndim = builder.module.get_or_insert_function(
         ir.FunctionType(ptr(int_), [ptr(llvm.ndt_ndarray_t)]),
         name="get_ndt_ndarray_t_ndim",
