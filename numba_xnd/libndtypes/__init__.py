16b2abdbb62b5c1bd57f7a8789c2dc3778dc395b
Saul Shanabrook <s.shanabrook@gmail.com>
2018-07-17 10:59:46 -0400

    Use portable loading for shared libraries in llvm
    from @pearu in https://github.com/Quansight/numba-xnd/pull/4/commits/15f8db9fac5ea61767af07cf26fb2dd1c1145338
    
    closes #4

--- numba_xnd/libndtypes/__init__.py
+++ numba_xnd/libndtypes/__init__.py
@@ -1,12 +1,8 @@
-import sys
 import llvmlite
+import ndtypes
+
 from .functions import *  # NOQA
 from .structs import *  # NOQA
 
 
-if sys.platform.startswith("linux"):
-    llvmlite.binding.load_library_permanently("libndtypes.so")
-elif sys.platform.startswith("darwin"):
-    llvmlite.binding.load_library_permanently("libndtypes.dylib")
-elif sys.platform.startswith("win"):
-    raise ImportWarning("Don't know how to load libndtypes library on windows")
+llvmlite.binding.load_library_permanently(ndtypes._ndtypes.__file__)
--- numba_xnd/libxnd/__init__.py
+++ numba_xnd/libxnd/__init__.py
@@ -1,13 +1,7 @@
-import sys
+import xnd
 import llvmlite
 
 # expose
 from .structs import *  # NOQA
 
-
-if sys.platform.startswith("linux"):
-    llvmlite.binding.load_library_permanently("libxnd.so")
-elif sys.platform.startswith("darwin"):
-    llvmlite.binding.load_library_permanently("libxnd.dylib")
-elif sys.platform.startswith("win"):
-    raise ImportWarning("Don't know how to load libxnd library on windows")
+llvmlite.binding.load_library_permanently(xnd._xnd.__file__)
